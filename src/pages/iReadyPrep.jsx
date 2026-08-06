b1adfa8ade52ea388c2845428e0b8c07a98faeff
Yohan Chang <yohanyinyuchang@gmail.com>
2026-08-05 22:18:51 -0700

    Update iframe source URL in IReadyPrep component

--- src/pages/iReadyPrep.jsx
+++ src/pages/iReadyPrep.jsx
@@ -3,7 +3,7 @@ export default function IReadyPrep() {
   return (
     <div className="flex flex-col" style={{ height: "calc(100vh - 60px)" }}>
       <iframe
-        src="https://ipath.base44.app"
+        src="https://ireadydiagnostic.vercel.app"
         title="iReady Diagnostic Prep"
         className="flex-1 w-full border-0"
         allow="fullscreen"
